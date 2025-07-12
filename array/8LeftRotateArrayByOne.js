1bbc5406a6fde5480a92e649a8ebaee3ef575a3f
Arun Kumar <kumar.arun52244@gmail.com>
2025-07-12 22:46:30 +0530

    Left rotate Array By One

--- array/8LeftRotateArrayByOne.js
+++ array/8LeftRotateArrayByOne.js
@@ -1,1 +1,17 @@
-const
+const arr=[0,2,4,7,1,9,-1,6,0,4,0];
+
+
+
+function leftRotateArayBy1(arr){
+    let ele=arr[0];
+
+    for(let i=0;i<arr.length -1;i++){
+        arr[i]=arr[i+1];
+    }
+    arr.pop();
+    arr.push(ele);
+
+    return arr
+}
+
+console.log("ans",leftRotateArayBy1(arr))
